f501f417e99e873d629a8bdcc375056fee003ed7
Dima <skydmytro@gmail.com>
2024-10-26 15:28:07 +0200

    feat( Hero Page): add title

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { DynamicBackgroundComponent } from "../components/DynamicBackgroundComponent";
+import { HeroPageTitle } from "../components/HeroPageTitle";
 export const HeroPage = () => {
   const [mounted, setMounted] = useState(false);
 
@@ -7,9 +8,13 @@ export const HeroPage = () => {
     setMounted(true);
   }, []);
 
+  const title = "Hero Page for G&M";
+  const subtitle = "Test Task by Skydan Dmytro";
+
   return (
     <div className="relative min-h-screen flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
       {mounted && <DynamicBackgroundComponent />}
+      <HeroPageTitle title={title} subtitle={subtitle} />
     </div>
   );
 };
